690a4428d241f02bc10ea1db8f2d6131935c05ec
jalagania <beka>
2023-04-01 17:54:37 +0400

    added tablet menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,44 @@
 import styles from "./Navigation.module.css";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { cartSlice } from "../store/cartSlice";
+import { useEffect, useState } from "react";
+import ThumbnailsSection from "./ThumbnailsSection";
 
 function Navigation({ location }) {
   const dispatch = useDispatch();
   const { setCartIsVisible } = cartSlice.actions;
   const { cartIsVisible } = useSelector((store) => store.cart);
 
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleMenu() {
+    setShowMenu(!showMenu);
+    dispatch(setCartIsVisible(false));
+  }
+
+  useEffect(() => {
+    document.body.style.overflow = showMenu ? "hidden" : "unset";
+  }, [showMenu]);
+
   return (
     <nav>
-      <img
-        src={
-          process.env.PUBLIC_URL + "/assets/shared/tablet/icon-hamburger.svg"
-        }
-        alt="menu"
-        className={styles.menuIcon}
-      />
-      <img
-        src={process.env.PUBLIC_URL + "/assets/shared/desktop/logo.svg"}
-        alt="logo"
-        className={styles.logo}
-      />
+      <button onClick={handleMenu}>
+        <img
+          src={
+            process.env.PUBLIC_URL + "/assets/shared/tablet/icon-hamburger.svg"
+          }
+          alt="menu"
+          className={styles.menuIcon}
+        />
+      </button>
+      <Link to="/">
+        <img
+          src={process.env.PUBLIC_URL + "/assets/shared/desktop/logo.svg"}
+          alt="logo"
+          className={styles.logo}
+        />
+      </Link>
       <ul className={styles.navLinks}>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -36,10 +53,24 @@ function Navigation({ location }) {
           <NavLink to="earphones">Earphones</NavLink>
         </li>
       </ul>
+      {showMenu && (
+        <div className={styles.menuModal}>
+          <div className={styles.menuWrapper}>
+            <ThumbnailsSection />
+          </div>
+          <div
+            className={styles.modalBg}
+            onClick={() => setShowMenu(false)}
+          ></div>
+        </div>
+      )}
       {location === "header" && (
         <button
           className={styles.cartIcon}
-          onClick={() => dispatch(setCartIsVisible(!cartIsVisible))}
+          onClick={() => {
+            dispatch(setCartIsVisible(!cartIsVisible));
+            setShowMenu(false);
+          }}
         >
           <img
             src={
